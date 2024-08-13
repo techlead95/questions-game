ef8accef46271f4c66a6cbec0331cdabd1fcbca4
techlead <techlead95@gmail.com>
2024-08-12 21:27:01 -0500

    Added prettier config and import order

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import router from "./router";
-import { Toaster } from "./components/ui/toaster";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import router from './router';
+import { Toaster } from './components/ui/toaster';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
     <Toaster />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
